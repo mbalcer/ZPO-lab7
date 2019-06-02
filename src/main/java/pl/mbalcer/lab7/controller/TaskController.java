c57142d5324c24b8a0eec374486a3b7a5362f350
mbalcer <mateusz75319@gmail.com>
2019-06-02 20:16:28 +0200

    Created method to getting tasks by user

--- src/main/java/pl/mbalcer/lab7/controller/TaskController.java
+++ src/main/java/pl/mbalcer/lab7/controller/TaskController.java
@@ -5,10 +5,13 @@ import org.springframework.web.bind.annotation.*;
 import pl.mbalcer.lab7.dto.CreateTaskDTO;
 import pl.mbalcer.lab7.dto.TaskDTO;
 import pl.mbalcer.lab7.entity.Task;
+import pl.mbalcer.lab7.enumType.TaskStatus;
+import pl.mbalcer.lab7.enumType.TaskType;
 import pl.mbalcer.lab7.mapper.TaskMapper;
 import pl.mbalcer.lab7.service.TaskService;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class TaskController {
@@ -28,5 +31,53 @@ public class TaskController {
     public void createTask(@RequestBody CreateTaskDTO taskDTO) {
         Task task = taskMapper.fromDTO(taskDTO);
         taskService.createTask(task);
+    }
+
+    @GetMapping("/tasks/user/{userId}")
+    public List<TaskDTO> getTasksByUser(@PathVariable long userId) {
+        List<Task> tasks = taskService.getTasks()
+                .stream()
+                .filter(task -> task.getUser().getId()==userId)
+                .collect(Collectors.toList());
+
+        return taskMapper.toDTOs(tasks);
+    }
+
+    @GetMapping("/tasks/user/{userId}/status/{status}")
+    public List<TaskDTO> getTasksByStatusAndUser(@PathVariable String status, @PathVariable long userId) {
+        TaskStatus taskStatus = TaskStatus.valueOf(status.toUpperCase());
+        List<Task> tasks = taskService.getTasks()
+                .stream()
+                .filter(task -> task.getUser().getId()==userId)
+                .filter(task -> task.getStatus()==taskStatus)
+                .collect(Collectors.toList());
+
+        return taskMapper.toDTOs(tasks);
+    }
+
+    @GetMapping("/tasks/user/{userId}/type/{type}")
+    public List<TaskDTO> getTasksByTypeAndUser(@PathVariable String type, @PathVariable long userId) {
+        TaskType taskType = TaskType.valueOf(type.toUpperCase());
+        List<Task> tasks = taskService.getTasks()
+                .stream()
+                .filter(task -> task.getUser().getId()==userId)
+                .filter(task -> task.getType()==taskType)
+                .collect(Collectors.toList());
+        
+        return taskMapper.toDTOs(tasks);
+    }
+
+    @GetMapping("/tasks/user/{userId}/status/{status}/type/{type}")
+    public List<TaskDTO> getTasksByTypeAndStatusAndUser(@PathVariable String status, @PathVariable String type, @PathVariable long userId) {
+        TaskType taskType = TaskType.valueOf(type.toUpperCase());
+        TaskStatus taskStatus = TaskStatus.valueOf(status.toUpperCase());
+        List<Task> tasks = taskService.getTasks()
+                .stream()
+                .filter(task -> task.getUser().getId()==userId)
+                .filter(task -> task.getType()==taskType)
+                .filter(task -> task.getStatus()==taskStatus)
+                .collect(Collectors.toList());
+
+        return taskMapper.toDTOs(tasks);
     }
 }
